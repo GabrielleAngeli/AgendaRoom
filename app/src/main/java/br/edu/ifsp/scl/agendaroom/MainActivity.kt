06806699d22db20e6d19a1af01a05613159ff1ef
Gabrielle <gabrielleangelipoli@gmail.com>
2023-12-08 20:48:22 -0300

    Ajustando dependencias do projeto

--- app/src/main/java/br/edu/ifsp/scl/agendaroom/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/agendaroom/MainActivity.kt
@@ -2,6 +2,7 @@ package br.edu.ifsp.scl.agendaroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.ifsp.agendaroom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
