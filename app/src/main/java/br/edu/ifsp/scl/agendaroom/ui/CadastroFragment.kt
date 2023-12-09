4efd7d06227a6545637129840d48d5b3c0ee2717
Gabrielle <gabrielleangelipoli@gmail.com>
2023-12-08 21:33:02 -0300

    criando navegação e ajustando fragments

---
+++ app/src/main/java/br/edu/ifsp/scl/agendaroom/ui/CadastroFragment.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.agendaroom.ui
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CadastroFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CadastroFragment : Fragment() {
+
+}
