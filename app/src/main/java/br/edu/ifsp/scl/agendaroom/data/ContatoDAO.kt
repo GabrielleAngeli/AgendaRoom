ac7298a9f931d1e1ada38a767ea6e37ee74c5663
Gabrielle <gabrielleangelipoli@gmail.com>
2023-12-08 21:55:02 -0300

    criando repository para pegar todos os contatos

--- app/src/main/java/br/edu/ifsp/scl/agendaroom/data/ContatoDAO.kt
+++ app/src/main/java/br/edu/ifsp/scl/agendaroom/data/ContatoDAO.kt
@@ -1,10 +1,16 @@
 package br.edu.ifsp.scl.agendaroom.data
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface ContatoDAO {
     @Insert
     suspend fun insert(contato: Contato)
+
+    @Query("SELECT * FROM contato ORDER BY nome")
+    fun getAllContacts(): LiveData<List<Contato>>
+
 }
--- app/src/main/java/br/edu/ifsp/scl/agendaroom/repository/ContatoRepository.kt
+++ app/src/main/java/br/edu/ifsp/scl/agendaroom/repository/ContatoRepository.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.agendaroom.repository
 
+import androidx.lifecycle.LiveData
 import br.edu.ifsp.scl.agendaroom.data.Contato
 import br.edu.ifsp.scl.agendaroom.data.ContatoDAO
 
@@ -7,4 +8,9 @@ class ContatoRepository (private val contatoDAO: ContatoDAO) {
     suspend fun insert(contato: Contato){
         contatoDAO.insert(contato)
     }
+
+    fun getAllContacts(): LiveData<List<Contato>> {
+        return contatoDAO.getAllContacts()
+    }
+
 }
